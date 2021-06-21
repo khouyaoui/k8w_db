5e215f5f75e4580a843fda175b4f178b57633bdb
root <root@LAPTOP-E4JFPVCA.localdomain>
2021-06-21 19:39:48 +0200

    1

---
+++ datos/cargaDatos.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Personas (
+    PersonaID int,
+    Nombre varchar(255),
+    Apellido1 varchar(255),
+    Apellido2 varchar(255),
+    Direccion varchar(255),
+    Poblacion varchar(255)
+);
+
+INSERT INTO Personas(PersonaID,Nombre,Apellido1,Apellido2,Direccion,Poblacion) VALUES 
+	(1,"Moha","Ben","-","Berlin 629","NCB");
+	
+INSERT INTO Personas(PersonaID,Nombre,Apellido1,Apellido2,Direccion,Poblacion) VALUES 
+	(2,"Binta","Anil","-","Gran Via, 11","GRT");
